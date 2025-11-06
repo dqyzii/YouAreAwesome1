8752ece6fdddef131e63c088574cbd10e2d04034
Daisy P <dqyzii@gmail.com>
2025-11-06 14:44:37 -0800

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,50 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by PERALES, DAISY on 11/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var message = "I Am A Programmer!"
+    var body: some View {
+        VStack {
+            Spacer()
+            
+            
+            Image(systemName: "swift")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.orange)
+                .frame(width: 200, height: 200)
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+            
+            Spacer()
+            Spacer()
+            
+            HStack {
+                Button("Awesome") {
+                    message = "Awesome!"
+                }
+                
+                Button("Great") {
+                    message = "Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
+            
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by PERALES, DAISY on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
